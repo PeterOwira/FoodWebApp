6eb9ddbbcd48d450068f0e77583facdce84c0ce3
PeterOwira <92856626+PeterOwira@users.noreply.github.com>
2022-05-19 12:00:29 +0300

    commit

---
+++ DATABASE FILE/kukito.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2022 at 10:57 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kukito`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about_us`
+--
+-- Error reading structure for table kukito.about_us: #1932 - Table 'kukito.about_us' doesn't exist in engine
+-- Error reading data for table kukito.about_us: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`about_us`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+-- Error reading structure for table kukito.admins: #1932 - Table 'kukito.admins' doesn't exist in engine
+-- Error reading data for table kukito.admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`admins`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bundle_product_relation`
+--
+-- Error reading structure for table kukito.bundle_product_relation: #1932 - Table 'kukito.bundle_product_relation' doesn't exist in engine
+-- Error reading data for table kukito.bundle_product_relation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`bundle_product_relation`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+-- Error reading structure for table kukito.cart: #1932 - Table 'kukito.cart' doesn't exist in engine
+-- Error reading data for table kukito.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`cart`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table kukito.categories: #1932 - Table 'kukito.categories' doesn't exist in engine
+-- Error reading data for table kukito.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_us`
+--
+-- Error reading structure for table kukito.contact_us: #1932 - Table 'kukito.contact_us' doesn't exist in engine
+-- Error reading data for table kukito.contact_us: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`contact_us`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupons`
+--
+-- Error reading structure for table kukito.coupons: #1932 - Table 'kukito.coupons' doesn't exist in engine
+-- Error reading data for table kukito.coupons: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`coupons`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+-- Error reading structure for table kukito.customers: #1932 - Table 'kukito.customers' doesn't exist in engine
+-- Error reading data for table kukito.customers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`customers`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_orders`
+--
+-- Error reading structure for table kukito.customer_orders: #1932 - Table 'kukito.customer_orders' doesn't exist in engine
+-- Error reading data for table kukito.customer_orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`customer_orders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enquiry_types`
+--
+-- Error reading structure for table kukito.enquiry_types: #1932 - Table 'kukito.enquiry_types' doesn't exist in engine
+-- Error reading data for table kukito.enquiry_types: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`enquiry_types`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manufacturers`
+--
+-- Error reading structure for table kukito.manufacturers: #1932 - Table 'kukito.manufacturers' doesn't exist in engine
+-- Error reading data for table kukito.manufacturers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`manufacturers`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+-- Error reading structure for table kukito.payments: #1932 - Table 'kukito.payments' doesn't exist in engine
+-- Error reading data for table kukito.payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`payments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_orders`
+--
+-- Error reading structure for table kukito.pending_orders: #1932 - Table 'kukito.pending_orders' doesn't exist in engine
+-- Error reading data for table kukito.pending_orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`pending_orders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table kukito.products: #1932 - Table 'kukito.products' doesn't exist in engine
+-- Error reading data for table kukito.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`products`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_categories`
+--
+-- Error reading structure for table kukito.product_categories: #1932 - Table 'kukito.product_categories' doesn't exist in engine
+-- Error reading data for table kukito.product_categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`product_categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+-- Error reading structure for table kukito.store: #1932 - Table 'kukito.store' doesn't exist in engine
+-- Error reading data for table kukito.store: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`store`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `terms`
+--
+-- Error reading structure for table kukito.terms: #1932 - Table 'kukito.terms' doesn't exist in engine
+-- Error reading data for table kukito.terms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`terms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+-- Error reading structure for table kukito.wishlist: #1932 - Table 'kukito.wishlist' doesn't exist in engine
+-- Error reading data for table kukito.wishlist: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kukito`.`wishlist`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
